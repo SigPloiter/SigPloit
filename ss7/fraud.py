c8bcf6af98b18af0711e09158c6aa20cf2025422
Loay Abdelrazek <SigPloiter@users.noreply.github.com>
2018-04-07 20:43:09 -0400

    added insertsubscriberdata function

--- ss7/fraud.py
+++ ss7/fraud.py
@@ -17,6 +17,7 @@ import ss7main
 simsi_path = os.path.join(os.getcwd(), 'ss7/attacks/fraud/simsi')
 mtsms_path = os.path.join(os.getcwd(), 'ss7/attacks/fraud/mtsms')
 cl_path = os.path.join(os.getcwd(), 'ss7/attacks/fraud/cl')
+isd_path = os.path.join(os.getcwd(),'ss7/attacks/fraud/isd')
 
 
 def simsi():
@@ -98,3 +99,31 @@ def cl():
         print "\033[31m[-]Error:\033[0mCancelLocation Failed to Launch, " + str(e)
         time.sleep(2)
         ss7main.Fraud()
+        
+def isd():
+	
+	jar_file = 'InsertSubscriberData.jar'
+
+	try:
+		insertSD = check_call(['java','-jar', os.path.join(isd_path,jar_file)])
+		if insertSD == 0:
+			fr = raw_input('\nWould you like to go back to Fraud Menu? (y/n): ')
+			if fr == 'y' or fr == 'yes':
+				ss7main.Fraud()
+			elif fr == 'n' or fr == 'no':
+				attack_menu = raw_input('Would you like to choose another attacks category? (y/n): ')
+				if attack_menu == 'y'or attack_menu =='yes':
+					ss7main.attacksMenu()
+				elif attack_menu == 'n' or attack_menu =='no':
+					main_menu = raw_input('Would you like to go back to the main menu? (y/exit): ')
+					if main_menu == 'y' or main_menu =='yes':
+						sigploit.mainMenu()
+					elif main_menu =='exit':
+						print 'TCAP End...'
+						sys.exit(0)
+			
+	
+	except CalledProcessError as e:
+		print "\033[31m[-]Error:\033[0mInsertSubscriberData Failed to Launch, " + str(e)
+		time.sleep(2)
+		ss7main.Fraud()
