0579e0b27f88c0a68f508dc7c16c69833ee9472d
Loay Abdelrazek <SigPloiter@users.noreply.github.com>
2018-07-07 06:47:08 -0400

    Update tracking.py

--- ss7/tracking.py
+++ ss7/tracking.py
@@ -18,6 +18,7 @@ sri_path = os.path.join(os.getcwd(), 'ss7/attacks/tracking/sri')
 srism_path = os.path.join(os.getcwd(), 'ss7/attacks/tracking/srism')
 psi_path = os.path.join(os.getcwd(), 'ss7/attacks/tracking/psi')
 ati_path = os.path.join(os.getcwd(), 'ss7/attacks/tracking/ati')
+srigprs_path = os.path.join(os.getcwd(), 'ss7/attacks/tracking/srigprs')
 
 
 def sri():
@@ -127,3 +128,28 @@ def ati():
         time.sleep(2)
         ss7main.ss7tracking()
 
+def srigprs():
+    jar_file = 'SendRoutingInfoForGPRS.jar'
+
+    try:
+        srigprs = check_call(['java', '-jar', os.path.join(srigprs_path, jar_file)])
+        if srigprs == 0:
+            lt = raw_input('\nWould you like to go back to LocationTracking Menu? (y/n): ')
+            if lt == 'y' or lt == 'yes':
+                ss7main.ss7tracking()
+            elif lt == 'n' or lt == 'no':
+                attack_menu = raw_input('Would you like to choose another attacks category? (y/n): ')
+                if attack_menu == 'y' or attack_menu == 'yes':
+                    ss7main.attacksMenu()
+                elif attack_menu == 'n' or attack_menu == 'no':
+                    main_menu = raw_input('Would you like to go back to the main menu? (y/exit): ')
+                    if main_menu == 'y' or main_menu == 'yes':
+                        sigploit.mainMenu()
+                    elif main_menu == 'exit':
+                        print 'TCAP End...'
+                        sys.exit(0)
+
+    except CalledProcessError as e:
+        print "\033[31m[-]Error:\033[0m" + jar_file + " Failed to Launch,  " + e.message
+        time.sleep(2)
+        ss7main.ss7tracking()
